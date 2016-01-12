75dab1d25a69ca4ab5c1d9e3e590f03946cbcffd
Edwin Hattink <edwinhattink@me.com>
2016-01-12 15:43:35 +0100

    BuildingFactory

---
+++ Machiavelli/Machiavelli/BuildingFactory.cpp
@@ -0,0 +1,12 @@
+#include "BuildingFactory.h"
+
+
+
+BuildingFactory::BuildingFactory()
+{
+}
+
+
+BuildingFactory::~BuildingFactory()
+{
+}
---
+++ Machiavelli/Machiavelli/BuildingFactory.h
@@ -0,0 +1,8 @@
+#pragma once
+class BuildingFactory
+{
+public:
+	BuildingFactory();
+	~BuildingFactory();
+};
+
